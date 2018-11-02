bf0daca38c34695400bb51b081be1e7aad4f1d2d
khanhdrag9 <39912487+khanhdrag9@users.noreply.github.com>
2018-11-02 17:14:08 +0700

    + Problem 8

---
+++ Problem8.py
@@ -0,0 +1,21 @@
+import time
+start = time.time()
+#program
+n = 4
+
+#get list number for file
+arrNumber = ""
+f = open("P8.txt", "rt")
+arrNumber = f.read()
+f.close()
+
+#set up
+
+
+
+#result
+
+
+#end program
+end = time.time()
+print ("Execute time : %s s" % str(end - start))
